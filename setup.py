72ec2a922b4f5944069af6a28fea8343e6cc7f70
Z.Shang <shangzhanlin@gmail.com>
2021-07-30 01:15:16 +1000

    init

---
+++ setup.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+
+VERSION = "0.0.1"
+
+DESCRIPTION = "Python module for simple pattern matching"
+CLASSIFIERS = [
+    "Intended Audience :: Developers",
+    "License :: OSI Approved :: MIT License",
+    "Natural Language :: English",
+    "Operating System :: OS Independent",
+    "Programming Language :: Python :: 3",
+    "Topic :: Software Development :: Libraries :: Python Modules",
+]
+
+
+def main():
+    try:
+        from setuptools import setup
+    except ImportError:
+        from distutils.core import setup
+
+    with open("README.mkd") as fin:
+        desc = fin.read().strip()
+
+    options = {
+        "name": "pyttern",
+        "version": VERSION,
+        "license": "MIT",
+        "description": DESCRIPTION,
+        "long_description": desc,
+        "url": "https://github.com/Z-Shang/pyttern",
+        "author": "zshang",
+        "author_email": "z@gilgamesh.me",
+        "classifiers": CLASSIFIERS,
+        "packages": [
+            "src",
+        ],
+    }
+    setup(**options)
+
+
+if __name__ == "__main__":
+    main()
